3089509a306846f882956b7495ceaaae9d6615ed
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-08-30 20:30:16 +0900

    Create PhpFileGenerator.php

---
+++ src/PhpFileGenerator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Koriym\Spaceman;
+
+class PhpFileGenerator
+{
+    public function __invoke(string $path) : \Generator
+    {
+        return $this->generate($path);
+    }
+
+    private function generate(string $path) : \Generator
+    {
+        foreach ($this->filesIterator($path) as $file) {
+            /* @var \SplFileInfo $file */
+            yield $file;
+        }
+    }
+
+    private function filesIterator(string $path) : \RegexIterator
+    {
+        return
+            new \RegexIterator(
+                new \RecursiveIteratorIterator(
+                    new \RecursiveDirectoryIterator(
+                        $path,
+                        \FilesystemIterator::CURRENT_AS_FILEINFO | \FilesystemIterator::KEY_AS_PATHNAME | \FilesystemIterator::SKIP_DOTS
+                    ),
+                    \RecursiveIteratorIterator::LEAVES_ONLY
+                ),
+                '/^.+\.php$/',
+                \RecursiveRegexIterator::MATCH
+            );
+    }
+}
